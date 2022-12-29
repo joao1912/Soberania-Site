f0afc2f4eade07af797bf3b49b0bb07b06c3f282
João Pedro T <joaopedrot1912@gmail.com>
2022-12-29 19:21:41 -0300

    changes

--- paginas/script.js
+++ paginas/script.js
@@ -1,4 +1,13 @@
+let quantJ1 = document.getElementById("quant1")
+let quantJ2 = document.getElementById("quant2")
+let quant
+
+
+
 function iniciar() {
     window.location.href = "pagina-jogoIniciado.html"
 }
 
+function QuantJogadores() {
+    
+}
