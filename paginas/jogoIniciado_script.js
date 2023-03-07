0b84a58f9e8f2ea58566f240b3728d650c35eb8d
João Pedro T <joaopedrot1912@gmail.com>
2023-03-07 15:44:57 -0300

    Update jogoIniciado_script.js

--- paginas/jogoIniciado_script.js
+++ paginas/jogoIniciado_script.js
@@ -3,10 +3,10 @@ let formulario = {
     cartasEvento: "",
     nomesPaises: []
 }
-let p1 = document.getElementById("pais1").value
-let p2 = document.getElementById("pais2").velue
-let p3 = document.getElementById("pais3").velue
-let p4 = document.getElementById("pais4").velue
+let p1 = document.getElementById("pais1").textContent
+let p2 = document.getElementById("pais2").textContent
+let p3 = document.getElementById("pais3").textContent
+let p4 = document.getElementById("pais4").textContent
 
 
 
@@ -42,7 +42,7 @@ let p4 = document.getElementById("pais4").velue
         document.querySelector("#playGame").addEventListener("click",function(){
             if(true) {
                 localStorage.setItem("formulario",JSON.stringify(formulario))
-                formulario.nomesPaises.push(p1, p2, p2, p4)
+                formulario.nomesPaises.push(p1) //esta indo vazil
                 console.log(formulario.nomesPaises)
                 //window.location.href = "pagina-jogoRodando.html"
             } else {
