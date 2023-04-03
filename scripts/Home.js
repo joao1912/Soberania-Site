a3ae47a4bbfd017cd03fca454ae6f980e5787042
joão <joaopedrot1912@gmail.com>
2023-04-03 00:31:06 -0300

    update 03/04
    alterando a pagina e navegação principal, refatorando.

---
+++ scripts/Home.js
@@ -0,0 +1,20 @@
+(function(){
+    document.getElementById("navContainer").addEventListener("click",function(event){
+        const navPost = event.target.id
+        switch(navPost) {
+            case "navHome":
+                
+                break
+            case "navComoUsar":
+
+                break
+            case "navCartas":
+
+                break
+            case "navJogar":
+
+                break
+            default:
+        }
+    })
+})()
