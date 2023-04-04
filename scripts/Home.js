246277625e032e8f9f931c6b588291412614916a
joão <joaopedrot1912@gmail.com>
2023-04-03 23:57:05 -0300

    refazendo o index

--- scripts/Home.js
+++ scripts/Home.js
@@ -1,17 +1,40 @@
 (function(){
     document.getElementById("navContainer").addEventListener("click",function(event){
         const navPost = event.target.id
+
+        const conteiners = document.getElementsByClassName("containersPrincipais")
+        const containerHome = document.getElementById("containerHome")
+        const containerComoUsar = document.getElementById("containerComoUsar")
+        const containerCartas = document.getElementById("containerCartas")
+        const containerJogar = document.getElementById("containerJogar")
+
+        function hideContainers() {
+            ;[...conteiners].forEach(function(container){
+                container.style.display = "none"
+
+            })
+            
+        }
+
         switch(navPost) {
             case "navHome":
-                
+                hideContainers()
+                containerHome.style.display = "block"
+
                 break
             case "navComoUsar":
+                hideContainers()
+                containerComoUsar.style.display = "block"
 
                 break
             case "navCartas":
+                hideContainers()
+                containerCartas.style.display = "block"
 
                 break
             case "navJogar":
+                hideContainers()
+                containerJogar.style.display = "block"
 
                 break
             default:
